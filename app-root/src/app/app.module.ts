aafdbee28ab80bc658c16f8a608a5db9baaf15bf
Cathy Yakomin <cyakomi1@villanova.edu>
2021-09-22 13:18:32 -0400

    home component created, top bar component created

--- app-root/src/app/app.module.ts
+++ app-root/src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
