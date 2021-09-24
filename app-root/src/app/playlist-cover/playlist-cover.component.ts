b35bb6acae34d3d552bcf7d732a86dff7d7eefa3
Cathy Yakomin <cyakomi1@villanova.edu>
2021-09-24 15:31:52 -0400

    Home component prints out several playlist components (hardcoded)

--- app-root/src/app/playlist-cover/playlist-cover.component.ts
+++ app-root/src/app/playlist-cover/playlist-cover.component.ts
@@ -3,8 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-playlist-cover',
   templateUrl: './playlist-cover.component.html',
-  styleUrls: ['./playlist-cover.component.css']
+  styleUrls: ['./playlist-cover.component.css'],
 })
+
 export class PlaylistCoverComponent implements OnInit {
 
   constructor() { }
