d11af06384b3dfd736b0af44b867637bc2061075
Olivia Boise <oboise@villanova.edu>
2021-10-04 15:01:51 -0400

    Created edit further component

--- app-root/src/app/app.module.ts
+++ app-root/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginModule } from '../login/login.module'
+import { LoginModule } from '../login/login.module';
+import { EditModule } from '../edit/edit.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule, LoginModule
+    BrowserModule, LoginModule, EditModule
   ],
   providers: [],
   bootstrap: [AppComponent]
