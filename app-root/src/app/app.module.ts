def632c04d9b68d621ea280c0c0367ce76e0af6d
Olivia Boise <oboise@villanova.edu>
2021-10-18 13:13:18 -0400

    Reformatting directories in login branch

--- app-root/src/app/app.module.ts
+++ app-root/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginModule } from '../login/login.module';
-import { EditModule } from '../edit/edit.module'
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule, LoginModule, EditModule
+    BrowserModule, LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
