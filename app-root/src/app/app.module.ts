e2fd0341e934df9cf6100de7fd61cb5561cf6dc3
Sebastian Flores <flores7029@gmail.com>
2021-10-18 23:29:53 -0400

    Routed Login and Home components

--- app-root/src/app/app.module.ts
+++ app-root/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { PlaylistCoverModule } from './playlist-cover/playlist-cover.module';
 import { HomeModule } from './home/home.module';
 import { TopbarModule } from './topbar/topbar.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, LoginModule, BrowserModule, FormsModule, ReactiveFormsModule, HomeModule, PlaylistCoverModule, TopbarModule
+    BrowserModule, AppRoutingModule,
+    LoginModule, BrowserModule, FormsModule, ReactiveFormsModule, HomeModule, PlaylistCoverModule, TopbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app-root/src/app/login/login.component.ts
+++ app-root/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   title = 'app-login'
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  clickEvent() {
+    this.router.navigate(['/home'])
   }
 
 }
