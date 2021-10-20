a19fa3808d33a214921aa634fb4b83f4a0cc2043
Olivia Boise <oboise@villanova.edu>
2021-10-20 09:24:03 -0400

    Configuring Edit further page for demo

--- app-root/src/app/app.module.ts
+++ app-root/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EditModule } from './edit/edit.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, EditModule
   ],
   providers: [],
   bootstrap: [AppComponent]
