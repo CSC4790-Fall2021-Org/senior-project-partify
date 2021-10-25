e219e4759a3c3a274686f44cd6be777ea6d7a484
Olivia Boise <oboise@villanova.edu>
2021-10-25 13:08:39 -0400

    Update edit further to no longer have hard-coded songs

--- app-root/src/app/edit/edit.component.ts
+++ app-root/src/app/edit/edit.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
+  songs: any [] = [
+    {name: "Song1"},
+    {name: "Song2"},
+    {name: "Song3"},
+    {name: "Song4"},
+    {name: "Song5"},
+    {name: "Song6"}
+  ]
+
+  recs: any [] = [
+    {name: "Song7"},
+    {name: "Song8"},
+    {name: "Song9"},
+  ]
   constructor() { }
 
   ngOnInit(): void {
