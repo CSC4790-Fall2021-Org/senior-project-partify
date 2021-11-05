c2aa7bce0e24a4b0fbef7adebbbc6e808175a53b
Olivia Boise <oboise@villanova.edu>
2021-11-05 13:01:04 -0400

    routing to edit further page from home now works

--- app-root/src/app/playlist-cover/playlist-cover.component.ts
+++ app-root/src/app/playlist-cover/playlist-cover.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-playlist-cover',
@@ -19,9 +20,26 @@ export class PlaylistCoverComponent implements OnInit {
     {name: "playlist8", img:"../../assets/images/playlistCover.PNG"},
   ]
 
-  constructor() { }
+  menuOption: any[] = [
+    {name: "Danceability"},
+    {name: "BPM"},
+    {name: "Camelot"},
+    {name: "Edit Further"}
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  // onChange(menuOption: any) {
+  //   console.log("Value: ", this.menuOption);
+  // }
+
+  onChange(value: any){
+    console.log(value);
+    if(value === 'Edit Further') {
+      this.router.navigate(['/edit']);
+    }
+  }
 }
